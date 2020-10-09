0a2b2004ad7d1cec856cc285c4f43b6d349d90a9
Mart Naris <mart.naris@hotmail.com>
2020-10-09 04:10:36 +0300

    jmockit version update + added tests

--- src/test/java/com/breakwater/task/permission/service/PermissionServiceTest.java
+++ src/test/java/com/breakwater/task/permission/service/PermissionServiceTest.java
@@ -1,20 +1,29 @@
 package com.breakwater.task.permission.service;
 
-import com.breakwater.task.department.dto.DepartmentDTO;
-import com.breakwater.task.department.model.Department;
-import com.breakwater.task.department.repository.DepartmentRepository;
-import com.breakwater.task.department.service.DepartmentService;
+import com.breakwater.task.exception.BusinessException;
+import com.breakwater.task.exception.NotFoundException;
+import com.breakwater.task.permission.dto.PermissionCreateDTO;
+import com.breakwater.task.permission.dto.PermissionDTO;
+import com.breakwater.task.permission.enums.PermissionCode;
+import com.breakwater.task.permission.enums.PermissionStatus;
+import com.breakwater.task.permission.model.Permission;
 import com.breakwater.task.permission.repository.PermissionRepository;
 import mockit.Expectations;
 import mockit.Injectable;
 import mockit.Tested;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public class PermissionServiceTest {
+
+    private static final UUID PERMISSION_ID = UUID.randomUUID();
+    private static final UUID DEPARTMENT_ID = UUID.randomUUID();
+    private static final UUID USER_ID = UUID.randomUUID();
 
     @Tested
     private PermissionService subject;
@@ -22,20 +31,118 @@ public class PermissionServiceTest {
     @Injectable
     private PermissionRepository permissionRepository;
 
-//    @Test
-//    public void permissionsFound() {
-//        var expectedParentDepartment = new DepartmentDTO(PARENT_ID, PARENT_ID.toString(), null, List.of());
-//        var expectedChildDepartment = new DepartmentDTO(CHILD_ID, CHILD_ID.toString(), expectedParentDepartment, List.of());
-//        new Expectations() {{
-//            departmentRepository.findById(CHILD_ID);
-//            result = Mono.just(new Department(CHILD_ID, CHILD_ID.toString(), PARENT_ID));
-//
-//            departmentRepository.findById(PARENT_ID);
-//            result = Mono.just(new Department(PARENT_ID, PARENT_ID.toString(), null));
-//        }};
-//
-//        StepVerifier.create(subject.readDepartmentWithParents(CHILD_ID))
-//                .expectNext(expectedChildDepartment)
-//                .verifyComplete();
-//    }
+    @Test
+    public void correctPermissionsFoundWhenNoDepartmentProvided() {
+        var expectedPermission = new PermissionDTO(PERMISSION_ID, PermissionCode.VIEW, DEPARTMENT_ID, USER_ID);
+        new Expectations() {{
+            permissionRepository.findByUserIdAndStatus(USER_ID, PermissionStatus.ACTIVE);
+            result = Flux.just(new Permission(PERMISSION_ID, PermissionCode.VIEW, PermissionStatus.ACTIVE,
+                    DEPARTMENT_ID, USER_ID));
+        }};
+
+        StepVerifier.create(subject.getPermissionsByUserIdAndDepartmentId(USER_ID, Optional.empty()))
+                .expectNext(expectedPermission)
+                .verifyComplete();
+    }
+
+    @Test
+    public void correctPermissionsFoundWhenDepartmentProvided() {
+        var expectedPermission = new PermissionDTO(PERMISSION_ID, PermissionCode.VIEW, DEPARTMENT_ID, USER_ID);
+        new Expectations() {{
+            permissionRepository.findByUserIdAndDepartmentIdAndStatus(USER_ID, DEPARTMENT_ID, PermissionStatus.ACTIVE);
+            result = Flux.just(new Permission(PERMISSION_ID, PermissionCode.VIEW, PermissionStatus.ACTIVE,
+                    DEPARTMENT_ID, USER_ID));
+        }};
+
+        StepVerifier.create(subject.getPermissionsByUserIdAndDepartmentId(USER_ID, Optional.of(DEPARTMENT_ID)))
+                .expectNext(expectedPermission)
+                .verifyComplete();
+    }
+
+    @Test
+    public void noElementsReturnedWhenNoPermissions() {
+        new Expectations() {{
+            permissionRepository.findByUserIdAndDepartmentIdAndStatus(USER_ID, DEPARTMENT_ID, PermissionStatus.ACTIVE);
+            result = Flux.empty();
+        }};
+
+        StepVerifier.create(subject.getPermissionsByUserIdAndDepartmentId(USER_ID, Optional.of(DEPARTMENT_ID)))
+                .expectNextCount(0)
+                .verifyComplete();
+    }
+
+    @Test
+    public void invalidPermissionIdThrowsException() {
+        new Expectations() {{
+            permissionRepository.findByUserIdAndIdAndStatus(USER_ID, PERMISSION_ID, PermissionStatus.ACTIVE);
+            result = Mono.empty();
+        }};
+
+        StepVerifier.create(subject.getUsersPermissionByPermissionId(USER_ID, PERMISSION_ID))
+                .expectError(NotFoundException.class)
+                .verify();
+    }
+
+    @Test
+    public void correctPermissionIdReturnsPermission() {
+        var expectedPermission = new PermissionDTO(PERMISSION_ID, PermissionCode.VIEW, DEPARTMENT_ID, USER_ID);
+        new Expectations() {{
+            permissionRepository.findByUserIdAndIdAndStatus(USER_ID, PERMISSION_ID, PermissionStatus.ACTIVE);
+            result = Mono.just(new Permission(PERMISSION_ID, PermissionCode.VIEW, PermissionStatus.ACTIVE,
+                    DEPARTMENT_ID, USER_ID));
+        }};
+
+        StepVerifier.create(subject.getUsersPermissionByPermissionId(USER_ID, PERMISSION_ID))
+                .expectNext(expectedPermission)
+                .verifyComplete();
+    }
+
+    @Test
+    public void unableToCreateSecondActivePermissionForUserAndDepartment() {
+        new Expectations() {{
+            permissionRepository.findByUserIdAndDepartmentIdAndPermissionCodeAndStatus(USER_ID, DEPARTMENT_ID,
+                    PermissionCode.VIEW, PermissionStatus.ACTIVE);
+            result = Mono.just(new Permission(PERMISSION_ID, PermissionCode.VIEW, PermissionStatus.ACTIVE,
+                    DEPARTMENT_ID, USER_ID));
+        }};
+
+        StepVerifier.create(subject.createUsersPermission(USER_ID, new PermissionCreateDTO(PermissionCode.VIEW,
+                DEPARTMENT_ID)))
+                .expectError(BusinessException.class)
+                .verify();
+    }
+
+    @Test
+    public void createNewActivePermissionForUserAndDepartment() {
+        var expectedPermission = new PermissionDTO(PERMISSION_ID, PermissionCode.VIEW, DEPARTMENT_ID, USER_ID);
+
+        var expectedPermissionEntity = new Permission(PERMISSION_ID, PermissionCode.VIEW, PermissionStatus.ACTIVE,
+                DEPARTMENT_ID, USER_ID);
+
+        new Expectations() {{
+            permissionRepository.findByUserIdAndDepartmentIdAndPermissionCodeAndStatus(USER_ID, DEPARTMENT_ID,
+                    PermissionCode.VIEW, PermissionStatus.ACTIVE);
+            result = Mono.empty();
+
+            permissionRepository.save(withAny(expectedPermissionEntity));
+            result = Mono.just(expectedPermissionEntity);
+        }};
+
+        StepVerifier.create(subject.createUsersPermission(USER_ID, new PermissionCreateDTO(PermissionCode.VIEW,
+                DEPARTMENT_ID)))
+                .expectNext(expectedPermission)
+                .verifyComplete();
+    }
+
+    @Test
+    public void revokeInvalidPermissionThrowsNotFoundException() {
+        new Expectations() {{
+            permissionRepository.findByUserIdAndIdAndStatus(USER_ID, PERMISSION_ID, PermissionStatus.ACTIVE);
+            result = Mono.empty();
+        }};
+
+        StepVerifier.create(subject.revokeUsersPermission(USER_ID, PERMISSION_ID))
+                .expectError(NotFoundException.class)
+                .verify();
+    }
 }
